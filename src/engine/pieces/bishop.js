b3fc8c5d1004b1b2552cb7da2610720bfa79e93d
GHSwallow <Gabriel.Swallow@softwire.com>
2023-11-08 16:03:32 +0000

    bishop can move diagonally

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,33 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        if (!currentSquare){ return new Array(0) }
+
+        let moves = new Array(0)
+        let differenceBetweenRowAndCol = Math.abs(currentSquare.row - currentSquare.col)
+        let sumOfRowAndCol = currentSquare.row + currentSquare.col
+
+        // forwards diagonal
+        if (currentSquare.row >= currentSquare.col){
+            for (let i = 0; i<8-differenceBetweenRowAndCol; i++) {
+                if (differenceBetweenRowAndCol+i !== currentSquare.row && i !== currentSquare.col){
+                    moves.push(Square.at(differenceBetweenRowAndCol+i, i))
+                }
+            }
+        } else {
+            for (let i = 0; i<8-differenceBetweenRowAndCol; i++){
+                if (i!==currentSquare.row && differenceBetweenRowAndCol+i!==currentSquare.col){
+                    moves.push(Square.at(i, differenceBetweenRowAndCol+i))
+                }
+            }
+        }
+
+        // backwards diagonal
+        for (let i = 0; i<=sumOfRowAndCol; i++){
+            if (i!==currentSquare.row && sumOfRowAndCol-i!==currentSquare.col)
+                moves.push(Square.at(i, sumOfRowAndCol-i))
+            }
+        return moves
     }
 }
