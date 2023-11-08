c5138ff630865364e86bcdd9fc82beec8f795d3e
GHSwallow <Gabriel.Swallow@softwire.com>
2023-11-08 15:07:47 +0000

    pawn moving one space implemented

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from "../square";
+import Player from "../player";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,15 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        if (!currentSquare){ return new Array(0) }
+
+        let moves = new Array(0)
+        if (this.player === Player.WHITE){
+            moves.push((Square.at(currentSquare.row + 1, currentSquare.col)))
+        } else if (this.player === Player.BLACK){
+            moves.push((Square.at(currentSquare.row - 1, currentSquare.col)))
+        }
+        return moves
     }
 }
