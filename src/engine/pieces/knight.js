51836d09d1b459b0a417f9f131d0cc1745c2a62a
GHSwallow <Gabriel.Swallow@softwire.com>
2023-11-08 16:37:15 +0000

    knight can move

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,24 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        if (!currentSquare){ return new Array(0) }
+        let moves = new Array(0)
+        moves.push(
+            Square.at(currentSquare.row+2, currentSquare.col+1),
+            Square.at(currentSquare.row+2, currentSquare.col-1),
+            Square.at(currentSquare.row-2, currentSquare.col+1),
+            Square.at(currentSquare.row-2, currentSquare.col-1),
+            Square.at(currentSquare.row+1, currentSquare.col+2),
+            Square.at(currentSquare.row+1, currentSquare.col-2),
+            Square.at(currentSquare.row-1, currentSquare.col+2),
+            Square.at(currentSquare.row-1, currentSquare.col-2),
+            )
+        moves.forEach((move) => {
+            if (move.row>7 || move.row<0 || move.col>7 || move.col<0){
+                moves.splice(moves.indexOf(move), 1)
+            }
+        })
+        return moves
     }
 }
