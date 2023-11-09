7ddd2af7872d3dc46194df7524a8b0b09d999b39
GHSwallow <Gabriel.Swallow@softwire.com>
2023-11-09 10:13:18 +0000

    delete unused functions (from refactor)

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -29,15 +29,6 @@ export default class Piece {
         return moves
     }
 
-    getAllLateralMoves(currentSquare) {
-        let moves = new Array(0)
-        for (let i = 0; i<8; i++) {
-            if (i!==currentSquare.col) {moves.push(Square.at(currentSquare.row, i))}
-            if (i!==currentSquare.row) {moves.push(Square.at(i, currentSquare.col))}
-        }
-        return moves;
-    }
-
     positionIsOnBoard(square){
         return (square.row >= 0 && square.row <= 7 && square.col >= 0 && square.col <= 7)
     }
@@ -48,36 +39,6 @@ export default class Piece {
             moves.push(move)
             move = Square.at(move.row+rowMove, move.col+colMove)
         }
-    }
-
-    getAllDiagonalMoves(currentSquare){
-        let row = currentSquare.row
-        let col = currentSquare.col
-        let moves = new Array(0)
-        let differenceBetweenRowAndCol = Math.abs(row - col)
-        let sumOfRowAndCol = row + col
-
-        // forwards diagonal
-        if (row >= col){
-            for (let i = 0; i<8-differenceBetweenRowAndCol; i++) {
-                if (differenceBetweenRowAndCol+i !== row && i !== col){
-                    moves.push(Square.at(differenceBetweenRowAndCol+i, i))
-                }
-            }
-        } else {
-            for (let i = 0; i<8-differenceBetweenRowAndCol; i++){
-                if (i!==row && differenceBetweenRowAndCol+i!==col){
-                    moves.push(Square.at(i, differenceBetweenRowAndCol+i))
-                }
-            }
-        }
-
-        // backwards diagonal
-        for (let i = 0; i<=sumOfRowAndCol; i++){
-            if (i!==row && sumOfRowAndCol-i!==col)
-                moves.push(Square.at(i, sumOfRowAndCol-i))
-        }
-        return moves
     }
 
     moveTo(board, newSquare) {
